adc5baf271e018e6453be6f249fb0281f206660c
Roger Bush <roger@grokible.com>
2016-03-06 00:19:33 +0000

    Added routing and styling ala tutorial section 5

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,25 @@
 import {Component} from 'angular2/core';
 import {HeroService} from './hero.service';
 import {HeroesComponent} from './heroes.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {DashboardComponent} from './dashboard.component';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
 @Component ({
   selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <a [routerLink]="['Heroes']">Heroes</a>
-    <router-outlet></router-outlet>
-  `,
-
+  templateUrl: "app/app.component.html",
+  styleUrls: ['app/app.component.css'],
   directives: [ROUTER_DIRECTIVES],
   providers: [ROUTER_PROVIDERS, HeroService]
 })
 
 
 @RouteConfig ([
-{
-    path: '/heroes',
-    name: 'Heroes',
-    component: HeroesComponent
-}
+    { path: '/dashboard', name: 'Dashboard', component: DashboardComponent,
+        useAsDefault: true },
+    { path: '/heroes', name: 'Heroes', component: HeroesComponent },
+    { path: '/detail/:id', name: 'HeroDetail',
+        component: HeroDetailComponent }
 ])
 
 export class AppComponent {
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,20 +1,27 @@
-import {Component} from 'angular2/core';
-import {Hero} from './hero';
+import { Component, OnInit } from 'angular2/core';
+import { RouteParams } from 'angular2/router';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-hero-detail',
   inputs: ['hero'],
-  template:`
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <div><input [(ngModel)]="hero.name" placeholder="name"></div>
-      </div>
-    </div>
-`,
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: ['app/hero-detail.component.css']
 })
+
 export class HeroDetailComponent {
-  hero: Hero;
+    constructor (private _heroService: HeroService,
+        private _routeParams: RouteParams) {}
+
+    hero: Hero;
+
+    ngOnInit () {
+        let id = +this._routeParams.get ('id');
+        this._heroService.getHero (id).then (hero => this.hero = hero);
+    }
+
+    goBack () {
+        window.history.back ();
+    }
 }
